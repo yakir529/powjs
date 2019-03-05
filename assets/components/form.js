94285d906e1325ce8f836951db8a8368521f5dbf
Yakir <yakir529@gmail.com>
2019-03-05 22:34:09 +0200

    added form handler component

--- assets/components/form.js
+++ assets/components/form.js
@@ -5,17 +5,41 @@ $pow.Components.form = (function(){
     var FormHandler = (function(){
         function FormHandler(){}
 
-        FormHandler.prototype.GetFormData = function(i_formSelector){
-            var _form = document.querySelector(i_formSelector),
-                o_formData = new FormData();
+        FormHandler.prototype.GetFormData = function (i_formElemUniqeSelector, i_dataToAddArr) {
+            var _form = document.querySelector(i_formElemUniqeSelector);
+            if (!_form || _form.tagName != "FORM") ConsoleManager.ThrowWarning("Element must be of type FORM");
+            else {
+                var _inputs = _form.querySelectorAll("input:not([type='button']), textarea"),
+                    _thisFormData = getFormDataObj();
 
-            if (_form){
-                var _dataList = _form.querySelectorAll("input, textarea, select"); 
-                _dataList.array.forEach(function(element) {
-                    //TODO: continue here
-
-                });
+                for (var i in _inputs) {
+                    if (!isNaN(i) && _inputs[i].hasAttribute("name")) {
+                        if (typeof _inputs[i].files != "undefined" && _inputs[i].files) {
+                            var _filesList = _inputs[i].files;
+                            for (var f in _filesList) {
+                                if (!isNaN(f)) {
+                                    _thisFormData.append(_inputs[i].getAttribute("name"), _filesList[f]);
+                                }
+                            }
+                        } else {
+                            _thisFormData.append(_inputs[i].getAttribute("name"), _inputs[i].value);
+                        }
+                    }
+                }
+                
+                if (typeof i_dataToAddArr != "undefined" && i_dataToAddArr.length) {
+                    for (var k in i_dataToAddArr) {
+                        if (typeof i_dataToAddArr[k].key != "undefined" && typeof i_dataToAddArr[k].value != "undefined") {
+                            _thisFormData.append(i_dataToAddArr[k].key, i_dataToAddArr[k].value);
+                        }
+                    }
+                }
+                return _thisFormData;
             }
+        }
+
+        function getFormDataObj() {
+            return new FormData();
         }
 
         return FormHandler;
